14b2ca73a030209966099008b6245739c14138bd
Albert Yi <albert@carbonfive.com>
2018-05-04 14:56:26 -0700

    add wait script

---
+++ script/testing/wait_for_web.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until curl http://$host ; do
+  echo "Danbooru web is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Danbooru web is up - executing command"
+exec $cmd
