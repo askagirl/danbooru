4cfb2830edb9c4d7dcf3c8567cee194d0b3ee2aa
Albert Yi <albert@carbonfive.com>
2018-05-04 15:36:59 -0700

    wip

--- script/testing/wait_for_web.sh
+++ script/testing/wait_for_web.sh
@@ -6,9 +6,9 @@ host="$1"
 shift
 cmd="$@"
 
-until curl -s http://$host ; do
+until docker exec danbooru_danbooru_1 curl -s http://$host ; do
   echo "Danbooru web is unavailable - sleeping"
-  sleep 1
+  sleep 3
 done
 
 >&2 echo "Danbooru web is up - executing command"
